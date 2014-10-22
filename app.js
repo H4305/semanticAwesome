49dd565f2d389be92bb13714c50180d0cf3eaaa2
Bernardo Rittmeyer <bernardo@rittme.com>
2014-10-22 11:34:19 +0200

    Added module spotlightRequest

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var express = require('express');
-var request = require('request');
+var spotlight = require('./spotlightRequest.js');
 var app = express();
 
 var text = "Nice is a large city in France on the French Riviera. It's a popular destination for vacationers both young and old, with something to offer nearly everyone. It is well known for the beautiful view on the Promenade des Anglais, its famous waterfront, and is an ethnically diverse port city. ";
 
+// Request routes
+
 app.get('/', function (req, res) {
-  requestDBPedia(text, function (body) {
-    res.send(body);
+  spotlight.getResources(text, function (URIList) {
+    var response = "";
+    URIList.forEach(function(URI) {
+      response += URI + "<br>";
+    });
+    res.send(response);
   });
 });
-
 
 var server = app.listen(3000, function () {
 
@@ -18,24 +23,4 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 
-})
-
-const DBPEDIA_SPOTLIGHT = "http://spotlight.dbpedia.org/rest/candidates";
-
-function requestDBPedia(text, callback) {
-
-  request({
-    uri: DBPEDIA_SPOTLIGHT+ "?text="+ encodeURI(text),
-    method: "GET",
-    timeout: 10000,
-    followRedirect: true,
-    headers: {
-      "Accept": "application/json"
-    },
-    maxRedirects: 10
-  }, function(error, response, body) {
-    console.log(error);
-    console.log(body);
-    callback.call(body);
-  });
-}
+});
---
+++ spotlightRequest.js
@@ -0,0 +1,85 @@
+var request = require('request');
+
+module.exports = (function() {
+
+  const RESOURCE_URI = "http://dbpedia.org/resource/";
+  const DBPEDIA_SPOTLIGHT = "http://spotlight.dbpedia.org/rest/candidates";
+  const BASE_CONFIDENCE = "0.3";
+
+  var confidence = BASE_CONFIDENCE;
+
+  /* requestSpotlight
+   * Sends a request to DBpedia spotlight.
+   * @param text the text argument
+   * @param callback the callback function to execute when ready
+   */
+
+  function requestSpotlight(text, callback) {
+
+    var args = "?text="+ encodeURI(text) + "&confidence=" + confidence;
+
+    request({
+      uri: DBPEDIA_SPOTLIGHT + args,
+      method: "GET",
+      timeout: 15000,
+      followRedirect: true,
+      headers: {
+        "Accept": "application/json"
+      },
+      maxRedirects: 3
+    }, function(error, response, body) {
+      console.log(error);
+      var responseObj = null;
+      if(!error) {
+        responseObj = JSON.parse(body);
+      }
+      callback(responseObj);
+    });
+  }
+
+  return {
+
+    setConfidence: function setConfidence(float) {
+      confidence = parseFloat(float, 10);
+    },
+
+    /* getResources
+     * Sends a request to DBpedia spotlight using requestSpotlight function.
+     * @param text The text to annotate
+     * @return A list with all the resource URIs
+     */
+
+    getResources: function getResources(text, callback) {
+      var resources = {};
+      //Sends the request 
+      requestSpotlight(text, function (object) {
+        if(object == null) {
+          console.log("Empty object cant be annotated.");
+          return null;
+        }
+        var annotation = object.annotation;
+        annotation.surfaceForm.forEach(function (data) {
+          if(data.resource) {
+            //If it's an array, length will not be undefined
+            if(data.resource.length !== undefined) {
+              data.resource.forEach(function (resource) {
+                //Push all the results as object keys (so they can be unique)
+                resources[resource["@uri"]] = true;
+              });
+            } else {
+              //Push the object result as an object key (so it can be unique)
+              resources[data.resource["@uri"]] = true;
+            }
+          }
+        });
+        var URIList = [];
+        //Iterates over all object keys and add them to the list
+        Object.keys(resources).forEach(function(resource) {
+          URIList.push(resource);
+        });
+        console.log(URIList);
+        callback(URIList);
+      });
+    }
+  }
+})();
