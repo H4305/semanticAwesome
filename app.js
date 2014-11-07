7dec498cdaea3508000f8ca24a2b5525cd144b74
Bernardo Rittmeyer <bernardo@rittme.com>
2014-11-07 21:44:34 +0100

    removed placeholder. Changed launch msg

--- app.js
+++ app.js
@@ -12,18 +12,7 @@ var mustacheExpress = require('mustache-express');
 app.route('/search').get(function(req,res,next){
 	makeRequest(req.query.q, function(results) {
 		console.log(results);
-		if(!results.length) {
-			results = [
-				["http://en.wikipedia.org/wiki/Nice_biscuit", 1, 2, '<b>Nice</b> biscuit - Wikipedia, the free encyclopedia',
-				'A <b>nice</b> biscuit is a coconut-flavoured biscuit. It is thin, rectangular in shape, with <br>\nrounded bumps on the edges, and lightly covered with a scattering of large sugar<br>\n&nbsp;...'],
-				['http://en.wikipedia.org/wiki/The_Nice', 2, 1, 'The <b>Nice</b> - Wikipedia, the free encyclopedia',
-				'The <b>Nice</b> were an English progressive rock band from the 1960s, known for their <br>\nblend of rock, jazz and classical music. Their debut album, The Thoughts of&nbsp;...'],
-				['http://en.wikipedia.org/wiki/Nice,_California', 3, 4, '<b>Nice</b>, California - Wikipedia, the free encyclopedia',
-				'<b>Nice</b> (formerly Clear Lake Villas) is a census-designated place (CDP) in Lake <br>\nCounty, California, United States. <b>Nice</b> is located 4.5 miles (7.2 km) southeast of&nbsp;...'],
-				['http://en.wikipedia.org/wiki/Treaty_of_Nice', 4, 3, 'Treaty of <b>Nice</b> - Wikipedia, the free encyclopedia',
-				'The Treaty of <b>Nice</b> was signed by European leaders on 26 February 2001 and <br>\ncame into force on 1 February 2003. It amended the Maastricht Treaty (or the&nbsp;...']
-			];
-		}
+		
 		var obj = {
 			query: req.query.q, 
 			results: results,
@@ -135,6 +124,6 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('SemanticAwesome listening at http://%s:%s', host, port);
 
 });
